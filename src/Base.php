529592161fe7d540525ceacf7f93719be3bc0428
wpsmith <t@wpsmith.net>
2019-02-20 19:32:07 -0500

    Update namespace/docs

--- src/Base.php
+++ src/Base.php
@@ -9,23 +9,23 @@
  * Any modifications to or software including (via compiler) GPL-licensed code must also be made
  * available under the GPL along with build & install instructions.
  *
- * @package    WPS\Capabilities
+ * @package    WPS\WP\Capabilities
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Capabilities;
+namespace WPS\WP\Capabilities;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Capabilities\Base' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\Base' ) ) {
 	/**
 	 * Capability Utils collection.
 	 */
--- src/CapabilityManagerInterface.php
+++ src/CapabilityManagerInterface.php
@@ -10,21 +10,21 @@
  *
  * @package    WPS\Capabilities
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Capabilities;
+namespace WPS\WP\Capabilities;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Capabilities\CapabilityManagerInterface' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\CapabilityManagerInterface' ) ) {
 	/**
 	 * Capability Manager interface.
 	 */
